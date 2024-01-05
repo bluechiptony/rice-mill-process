96fa27e486f6df770386341296efe650e85bcdc5
bluechiptony <tony5egwu@gmail.com>
2024-01-05 13:50:41 +0100

    swagger
    basic implementation of swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('COREN Hub API')
+    .setVersion('1.0')
+    .setDescription('COREN Hub Rest API')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(8083);
 }
 bootstrap();
